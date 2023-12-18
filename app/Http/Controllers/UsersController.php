1b7e0a64dee77e0cb6410758a88da3e75cd5d0f8
Deni Share <denifenom@gmail.com>
2023-12-18 12:39:24 +0700

    first up

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\DataTables\UsersDataTable;
+
+class UsersController extends Controller
+{
+    public function index(UsersDataTable $dataTable)
+    {
+        return $dataTable->render('users.index');
+    }
+
+    public function create()
+    {
+        return "Silahkan Buat Form";
+    }
+}
